4deaf56fa6380530aadc7412bc4595fa97aa89c3
Avery Angel <avery.jordan.angel@gmail.com>
2021-07-14 11:10:42 -0700

    clink
    added clink effect.
    still need to design it for mobile.
    freshened up the design with some borders also...

--- src/Components/About/SkillList.js
+++ src/Components/About/SkillList.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import Coin from '../../Assets/coin.svg'
+import CoinBase from '../../Assets/coin.svg'
 import CoinSlotLeft from '../../Assets/coinslotLeft.svg'
 import CoinSlotRight from '../../Assets/coinslotRight.svg'
+import CoinSlotClink from '../../Assets/coinslotClink.svg'
 import '../../App.css'
 
 class SkillList extends React.Component {
   constructor() {
     super()
-    this.state = {
-    }
+    this.state = {}
   }
 
   render() {
@@ -17,9 +17,12 @@ class SkillList extends React.Component {
         <div className="skillListContent">
           <h1 className="skillTitle">Skills</h1>
           <div className="skillListContain">
-            <img src={Coin} className='coin' alt='coin'/>
-            <img src={CoinSlotLeft} className='coinslotLeft' alt='coinslot'/>
-            <img src={CoinSlotRight} className='coinslotRight' alt='coinslot'/>
+            <div className="coinContain">
+              <img src={CoinBase} className='coin' alt='coin'/>
+              <img src={CoinSlotLeft} className='coinslotLeft' alt='coinslot'/>
+              <img src={CoinSlotRight} className='coinslotRight' alt='coinslot'/>
+              <img src={CoinSlotClink} className='coinslotClink' alt='coinslotClink'/>
+            </div>
             <h3 className="skillIntro">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
             <div className="skillContain">
               <h2 className="skillName">Lorem ipsum</h2>
