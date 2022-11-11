93ba74703b2ffbfa2c5f4a020567cbcdf471f721
aja <avery.jordan.angel@gmail.com>
2022-11-10 23:19:42 -0700

    Splash Redesign
    Took another whack at a splash screen.
    
    - Need to make it work on landscape for mobile devices.
    
    - Need to check performance
    
    - Need to continue with placement for input Icons
    
    - Don't be so hard on yourself

--- src/Components/About/Splash.js
+++ src/Components/About/Splash.js
@@ -2,12 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getSplashOffset, isSetToContact } from '../../Redux/Actions/yPosController.js';
-import { ReactComponent as InputsBackground } from '../../Assets/inputsBackground.svg';
-import { ReactComponent as LightPunch } from '../../Assets/inputsLP.svg';
-import { ReactComponent as HeavyPunch } from '../../Assets/inputsHP.svg';
-import { ReactComponent as LightKick } from '../../Assets/inputsLK.svg';
-import { ReactComponent as HeavyKick } from '../../Assets/inputsHK.svg';
-import { ReactComponent as Arrow } from '../../Assets/downArrow.svg';
+import { ReactComponent as Icon } from '../../Assets/heroIcon.svg';
+import Me from '../../Assets/newHeroMe.png';
 import '../../App.css'
 
 class Splash extends React.Component {
@@ -37,30 +33,24 @@ class Splash extends React.Component {
   render() {
     return (
       <div id="splash" className="splashBackground">
-        <div className="splashContain">
-          <div className="catchTextContain">
-            <h1 className='greetings'>Hi, I'm Avery.</h1>
-            <h4>
-              I am a UX / UI Designer. My experience includes leading and managing 
-              teams of full stack web developers and graphic designers that create AI driven e-commerce platforms, 
-              mobile applications, frontend design standards, company logos, and websites.
-            </h4>
-          </div>
-          <div className="splashSVGContain">
-            <InputsBackground className='inputBackground' />
-            <LightPunch className='inputs LP' />
-            <HeavyPunch className='inputs HP' />
-            <LightKick className='inputs LK' />
-            <HeavyKick className='inputs HK' />
-          </div>
-          <div className="contactLinkContain">
-            <Link onClick={() => this.props.isSetToContact(true)} className='link' to='/'>
-              <h1 className="fade contactLink splashLink">Let's Talk</h1>
-            </Link>
-          </div>
+        <div className="splashCatchContain">
+          <h1>Hi, I'm Avery.</h1>
+          <h4>
+            I am a UX / UI Designer. My experience includes leading and managing 
+            teams of full stack web developers and graphic designers that create AI driven e-commerce platforms, 
+            mobile applications, frontend design standards, company logos, and websites.
+          </h4>
         </div>
-        <div className="arrowContain">
-          <Arrow className='arrow' />
+        <div className="contactLinkContain">
+          <Link onClick={() => this.props.isSetToContact(true)} className='link' to='/'>
+            <h1 className="fade contactLink splashLink">Let's Talk</h1>
+          </Link>
+        </div>
+        <div className="splashAssetBackground">
+          <div className="splashAssetContain">
+            <Icon className="heroIcon"/>
+            <img src={Me} className="me" alt="Myself"/>
+          </div>
         </div>
       </div>
     );
